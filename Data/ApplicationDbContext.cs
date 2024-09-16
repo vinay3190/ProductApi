4467c78f8a713526b7588e68f5061811ae64f8e0
vinay3190 <96431867+vinay3190@users.noreply.github.com>
2024-09-16 16:08:58 +0530

    Create ApplicationDbContext.cs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Models;
+
+namespace ProductApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}

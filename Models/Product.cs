fad02b21e35d29124ee08b72776d3d83d775f290
vinay3190 <96431867+vinay3190@users.noreply.github.com>
2024-09-16 16:06:08 +0530

    Fields added

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,1 +1,19 @@
 
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductApi.Models
+{
+    public class Product
+    {
+        [Key]
+        [Range(100000, 999999)]  // Ensures ProductId is a 6-digit number
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
